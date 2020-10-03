10bd8e7ab339ff4ed660053356464e8509dbc7fa
usfbelhadj <usf.belhadj@gmail.com>
2020-10-03 12:49:08 +0100

    Fix pep8

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
+
 class square():
-    
+
     width = 0
     height = 0
-    
+
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
-        if 'width'in kwargs.keys() and self.height != self.width:
-                self.height = self.width
-        if 'height'in kwargs.keys() and self.width != self.height:
-                self.width = self.height           
+        if 'width' in kwargs.keys() and self.height != self.width:
+            self.height = self.width
+        if 'height' in kwargs.keys() and self.width != self.height:
+            self.width = self.height
 
     def area_of_my_square(self):
         """ Area of the square """
@@ -23,9 +24,10 @@ class square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
 
-    s = square( height=9)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
