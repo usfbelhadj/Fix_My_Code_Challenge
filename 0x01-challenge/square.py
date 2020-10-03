71148dd9605b065993e66f251afe187abc068058
usfbelhadj <usf.belhadj@gmail.com>
2020-10-03 12:54:40 +0100

    Com

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+'''square Class'''
 
 
 class Square():
+    '''Class Square'''
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        '''Constructor'''
         for key, value in kwargs.items():
             setattr(self, key, value)
         if 'width' in kwargs.keys() and self.height != self.width:
