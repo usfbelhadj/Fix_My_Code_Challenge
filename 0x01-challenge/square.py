bd4962a3e0370ef86d866e31edb6b1966b833baa
usfbelhadj <usf.belhadj@gmail.com>
2020-10-03 12:46:25 +0100

    Testt

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,15 +1,17 @@
-
 #!/usr/bin/python3
 
 class square():
     
     width = 0
     height = 0
-
     
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+        if 'height'in kwargs.keys() and self.width != self.height:
+                self.width = self.height           
+        if 'width'in kwargs.keys() and self.height != self.width:
+                self.height = self.width
 
     def area_of_my_square(self):
         """ Area of the square """
@@ -23,7 +25,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square( height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
